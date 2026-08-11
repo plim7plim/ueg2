25ca72a3643006da01666c88cf766fc9b0f4e52f
ViniciosBrandao <vhfbrandao@gmail.com>
2026-08-11 20:22:24 -0300

    odeiojs

---
+++ exercicio.js
@@ -0,0 +1,41 @@
+const prompt = require("prompt-sync")();
+
+let soma = 0;
+
+let maior = 0;
+let nomeMaior = "";
+let menor = Infinity;
+let nomeMenor="";
+
+function calculaMedia(){
+    for (let i = 1; i<=15; i++){
+        let nome = prompt("nome : " + i + " : ")
+        let nota =  Number(prompt("Digite a nota do aluno: " + nome + " : "));
+        
+        console.log("Nome: " + nome);
+
+        soma = soma + nota;
+    
+        if (nota > maior){
+            maior = nota //seta maior
+            nomeMaior=nome;
+        }
+        if (nota<menor){
+            menor = nota //seta menor
+            nomeMenor = nome;
+        }
+        media = soma/15;
+    }
+    
+    
+    console.log("maior " + maior);
+    console.log("Maior aluno: " + nomeMaior);
+
+    console.log("menor " + menor);
+    console.log("Menor aluno: " + nomeMenor);
+
+    console.log("media " + media);
+
+}
+
+calculaMedia();
